627fddc6ab844bf0a75c3a1db673f62577d1fd99
Alex Zverev <alexzverev2448@gmail.com>
2023-05-08 16:03:41 +0300

    crashtest

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,6 +11,12 @@ const auth = require('../middlewares/auth');
 
 const NotFoundError = require('../errors/NotFoundError');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
