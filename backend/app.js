45c2f420c2efa64666b37ed59e1bf7c90c62314e
Alex Zverev <alexzverev2448@gmail.com>
2023-05-09 12:10:26 +0300

    dotenv

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const { errors } = require('celebrate');
 
 const handleErrors = require('./middlewares/handleErrors');
