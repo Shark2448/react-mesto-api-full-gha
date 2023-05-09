37ac1722aaf5a684d0aa36744cd5dc9f83b56bbd
Alex Zverev <alexzverev2448@gmail.com>
2023-05-09 14:52:25 +0300

    тест

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,7 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
